c1b2f01efe00b5edafbd5207c32477dbb09adeb2
Collin L <lkangn.collin@gmail.com>
2025-04-23 18:00:47 +0800

    feat(linux): update netlink codedemo

--- Linux/KernelMoudle/netlink/user-client_kernel-server/netlink_kernel.c
+++ Linux/KernelMoudle/netlink/user-client_kernel-server/netlink_kernel.c
@@ -3,7 +3,7 @@
 #include <linux/netlink.h>
 #include <linux/skbuff.h>
 
-#define NETLINK_TEST 30
+#define NETLINK_TEST 60
 
 static struct sock *nl_sk = NULL;
 
@@ -18,6 +18,8 @@ static struct sock *nl_sk = NULL;
 int test_unicast(void *data, size_t size, __u32 pid)
 {
     struct sk_buff *skb_out;
+
+    // 以size作为数据长度, 申请一个新的netlink消息大小的sk_buff
     skb_out = nlmsg_new(size, GFP_ATOMIC);
     if(!skb_out) {
         printk(KERN_ERR "Failed to allocate a new sk_buff\n");
@@ -26,10 +28,25 @@ int test_unicast(void *data, size_t size, __u32 pid)
 
     //struct nlmsghdr* nlmsg_put(struct sk_buff *skb, u32 portid, u32 seq, int type, int len, int flags);
     struct nlmsghdr * nlh;
+
+    /*
+     * 添加netlink消息头部, 表示消息的类型、长度、序列号等
+     *
+     * nlmsg_put - Add a new netlink message to an skb
+     * @skb: socket buffer to store message in
+     * @portid: netlink PORTID of requesting application
+     * @seq: sequence number of message
+     * @type: message type
+     * @payload: length of message payload
+     * @flags: message flags
+     */
     nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, size, 0);
 
+    // 拷贝数据到netlink的data区
     memcpy(nlmsg_data(nlh), data, size);
 
+    // 配置目的地址
+    //
     // 设置 SKB 的控制块（CB）
     // 控制块是 struct sk_buff 结构特有的，用于每个协议层的控制信息（如：IP层、TCP层）
     // 对于 Netlink 来说，其控制信息是如下结构体：
@@ -99,9 +116,12 @@ static void nl_recv_msg(struct sk_buff *skb)
 {
     struct nlmsghdr *nlh = (struct nlmsghdr*)skb->data;
     char *data = "Hello userspace";
-    printk(KERN_INFO "==== LEN(%d) TYPE(%d) FLAGS(%d) SEQ(%d) PORTID(%d)\n", nlh->nlmsg_len, nlh->nlmsg_type,
+
+    printk(KERN_INFO "====[KERNEL] LEN(%d) TYPE(%d) FLAGS(%d) SEQ(%d) PORTID(%d)\n", nlh->nlmsg_len, nlh->nlmsg_type,
            nlh->nlmsg_flags, nlh->nlmsg_seq, nlh->nlmsg_pid);
+
     printk("Kernel Received %d bytes: %s\n", nlmsg_len(nlh), (char*)nlmsg_data(nlh));
+
     test_unicast(data, strlen(data), nlh->nlmsg_pid);
 }
 
@@ -149,6 +169,8 @@ static int __init test_init(void)
     */
 
     //This is for 3.8 kernels and above.
+
+    // 注册 netlink 协议, NETLINK_TEST
     struct netlink_kernel_cfg cfg = {
         .input = nl_recv_msg,
     };
--- Linux/KernelMoudle/netlink/user-client_kernel-server/netlink_user.c
+++ Linux/KernelMoudle/netlink/user-client_kernel-server/netlink_user.c
@@ -10,7 +10,6 @@
 #define MAX_PAYLOAD     1024    /* maximum payload size*/
 #define MAX_NL_BUFSIZ   NLMSG_SPACE(MAX_PAYLOAD)
 
-//int PORTID = getpid();
 int PORTID = 1;
 
 int create_nl_socket(uint32_t pid, uint32_t groups)
@@ -78,18 +77,42 @@ int nl_sendto(int fd, void *buffer, size_t size, uint32_t pid, uint32_t groups)
         return -1;
     }
 
+    // 目的地址信息
     struct sockaddr_nl addr;
     memset(&addr, 0, sizeof(addr));
     addr.nl_family = AF_NETLINK;
     addr.nl_pid = pid;          /* Send messages to the linux kernel. */
     addr.nl_groups = groups;    /* unicast */
 
+/*
+   消息格式：
+	 <--- nlmsg_total_size(payload)  --->
+	 <-- nlmsg_msg_size(payload) ->
+	+----------+- - -+-------------+- - -+------------
+	| nlmsghdr | Pad |   Payload   | Pad | nlmsghdr
+	+----------+- - -+-------------+- - -+------------
+	nlmsg_data(nlh)---^                   ^
+	nlmsg_next(nlh)-----------------------+
+
+   消息头结构体：
+	struct nlmsghdr {
+	    __u32       nlmsg_len;     
+	    __u16       nlmsg_type;    
+	    __u16       nlmsg_flags;   
+	    __u32       nlmsg_seq;     
+	    __u32       nlmsg_pid;     
+	};
+*/
     // 设置 Netlink 消息缓冲区
     nlh = (struct nlmsghdr *)&nl_tmp_buffer;
     memset(nlh, 0, MAX_NL_BUFSIZ);
     nlh->nlmsg_len = NLMSG_LENGTH(size);
     nlh->nlmsg_pid = PORTID;
     memcpy(NLMSG_DATA(nlh), buffer, size);
+
+    printf("====[USER] LEN(%d) TYPE(%d) FLAGS(%d) SEQ(%d) PID(%d)\n", nlh->nlmsg_len, nlh->nlmsg_type,
+           nlh->nlmsg_flags, nlh->nlmsg_seq, nlh->nlmsg_pid);
+    printf("User send %d bytes: %s\n", size, (char *)buffer);
 
     return sendto(fd, nlh, NLMSG_LENGTH(size), 0, (struct sockaddr *)&addr, sizeof(addr));
 
@@ -107,29 +130,46 @@ int nl_sendto(int fd, void *buffer, size_t size, uint32_t pid, uint32_t groups)
 
 int main(void)
 {
-    char data[] = "Hello kernel";
-    int sockfd = create_nl_socket(PORTID, 0);
-    if (sockfd == -1) {
-        return 1;
+    char data[] = "Hello kernel server, this is user client";
+    int sock_fd= 0;
+    int ret = 0;
+    struct sockaddr_nl addr={};
+
+    // 创建 Netlink 套接字
+    sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_TEST);
+    if (sock_fd == -1) {
+        return -1;
     }
 
-    int ret;
-    ret = nl_sendto(sockfd, data, sizeof(data), 0, 0);
+    memset(&addr, 0, sizeof(addr));
+    addr.nl_family = AF_NETLINK;
+    addr.nl_pid = PORTID;
+    addr.nl_groups = 0;
+
+    // 绑定自身 Netlink 套接字相关信息
+    if (bind(sock_fd, (struct sockaddr *)&addr, sizeof(addr)) != 0) {
+        close(sock_fd);
+        return -1;
+    }
+
+    // 发送 Netlink 消息
+    ret = nl_sendto(sock_fd, data, sizeof(data), 0, 0);
     if (ret < 0) {
         printf("Fail to send\n");
         return 1;
     }
-    printf("Sent %d bytes\n", ret);
+    printf("====[USER] Sent %d bytes in totals\n\n", ret);
 
-    ret = nl_recv(sockfd);
+    // 接收 Netlink 回复消息
+    ret = nl_recv(sock_fd);
     if (ret < 0) {
         printf("Fail to receive\n");
     }
-    printf("Received %d bytes\n", ret);
+    printf("====[USER] Received %d bytes\n", ret);
 
     // while (1) {
-    // nl_recv(sockfd);
-    // nl_sendto(sockfd, data, sizeof(data), 0, 0);
+    // nl_recv(sock_fd);
+    // nl_sendto(sock_fd, data, sizeof(data), 0, 0);
     // }
 
     return 0;
