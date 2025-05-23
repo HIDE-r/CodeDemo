5cecd5b4004c044989a19362787515e758bdc265
Collin L <lkangn.collin@gmail.com>
2025-05-23 09:28:00 +0800

    feat(C): update comment

--- C/Tools/sanitise_stdfd.c
+++ C/Tools/sanitise_stdfd.c
@@ -15,14 +15,18 @@ sanitise_stdfd(void)
 {
 	int nullfd, dupfd;
 
+	// 尝试打开一个文件获取文件描述符
 	if ((nullfd = dupfd = open(_PATH_DEVNULL, O_RDWR)) == -1) {
 		fprintf(stderr, "Couldn't open /dev/null: %s\n",
 		    strerror(errno));
 		exit(1);
 	}
 
-	// 如果发现 0 1 2 是没有打开的文件描述符, 则指向 /dev/null
+	// 如果发现刚打开的文件描述符小于 2, 则指向 /dev/null
+	// 目的是为了保证 0 1 2 都是有效的文件描述符
 	while (++dupfd <= STDERR_FILENO) {
+
+		// 检查文件描述符是否有效
 		/* Only populate closed fds. */
 		if (fcntl(dupfd, F_GETFL) == -1 && errno == EBADF) {
 			if (dup2(nullfd, dupfd) == -1) {
@@ -31,6 +35,8 @@ sanitise_stdfd(void)
 			}
 		}
 	}
+
+	// 如果发现刚打开的文件描述符大于 2, 则关闭它
 	if (nullfd > STDERR_FILENO)
 		close(nullfd);
 }
